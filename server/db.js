d2d202819000adb6498e5bdad3f52670612852d7
subrat-ranjan <swainsubrat975@gmail.com>
2023-06-18 16:04:22 +0530

    db

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 
-const mongoUri = "mongodb://localhost:27017/inotebook"
+const mongoUri = "mongodb+srv://subratranjan400:TzLjBJYJEgIaQG9y@cluster0.4xmzzvr.mongodb.net/inotebook"
+
 
 const connectToMongo = () => {
     mongoose.connect(mongoUri, () => {
